5553c8f0062a8c670f75fb63f98c47c506f05161
Saša Savić <sasa@sasa-savic.com>
2019-03-07 22:08:33 +0100

    Use base16 summerfruit colors

--- config.h
+++ config.h
@@ -5,11 +5,17 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {"mono:size=11:antialias=true:autohint=true"};
 static const char *prompt  = NULL;      /* -p  option; prompt to the left of input field */
+
+static const char prim_bg[]         = "#ffffff";
+static const char prim_hl[]         = "#1faaaa";
+static const char secd_bg[]         = "#e0e0e0";
+static const char prim_tx[]         = "#151515";
+
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#000000" },
-	[SchemeSel] = { "#ffffff", "#444444" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { prim_tx, prim_bg },
+	[SchemeSel] = { prim_hl, secd_bg },
+	[SchemeOut] = { prim_bg, prim_hl },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
