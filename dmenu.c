c03fd81b7a519188b61cb12d547678ce7d2c12c3
Saša Savić <sasa@sasa-savic.com>
2019-11-17 14:30:41 +0100

    Revert to base
    This reverts commit e8bad9830e8e10c5593bc8d672d280df8065f565.

--- dmenu.c
+++ dmenu.c
@@ -31,7 +31,6 @@ enum { SchemeNorm, SchemeSel, SchemeOut, SchemeLast }; /* color schemes */
 struct item {
 	char *text;
 	struct item *left, *right;
-    int distance;
 	int out;
 };
 
@@ -211,84 +210,59 @@ grabkeyboard(void)
 	die("cannot grab keyboard");
 }
 
-static int
-compare_distance(const void *a, const void *b)
-{
-	struct item const *da = *(struct item **) a;
-	struct item const *db = *(struct item **) b;
-
-	if (!db)
-		return 1;
-	if (!da)
-		return -1;
-	return da->distance - db->distance;
-}
-
 static void
-fuzzymatch(void)
+match(void)
 {
-	struct item *item;
-	struct item **fuzzymatches = NULL;
-	char c;
-	int number_of_matches = 0, i, pidx, sidx, eidx;
-	int text_len = strlen(text), itext_len;
+	static char **tokv = NULL;
+	static int tokn = 0;
 
-	matches = matchend = NULL;
+	char buf[sizeof text], *s;
+	int i, tokc = 0;
+	size_t len, textsize;
+	struct item *item, *lprefix, *lsubstr, *prefixend, *substrend;
 
-	/* walk through all items */
+	strcpy(buf, text);
+	/* separate input text into tokens to be matched individually */
+	for (s = strtok(buf, " "); s; tokv[tokc - 1] = s, s = strtok(NULL, " "))
+		if (++tokc > tokn && !(tokv = realloc(tokv, ++tokn * sizeof *tokv)))
+			die("cannot realloc %u bytes:", tokn * sizeof *tokv);
+	len = tokc ? strlen(tokv[0]) : 0;
+
+	matches = lprefix = lsubstr = matchend = prefixend = substrend = NULL;
+	textsize = strlen(text) + 1;
 	for (item = items; item && item->text; item++) {
-		if (text_len) {
-			itext_len = strlen(item->text);
-			pidx = 0;
-			sidx = eidx = -1;
-			/* walk through item text */
-			for (i = 0; i < itext_len && (c = item->text[i]); i++) {
-				/* fuzzy match pattern */
-				if (text[pidx] == c) {
-					if (sidx == -1)
-						sidx = i;
-					pidx++;
-					if (pidx == text_len) {
-						eidx = i;
-						break;
-					}
-				}
-			}
-			/* build list of matches */
-			if (eidx != -1) {
-				/* compute distance */
-				/* factor in 30% of sidx and distance between eidx and total
-				 * text length .. let's see how it works */
-				item->distance = eidx - sidx + (itext_len - eidx + sidx) / 3;
-				appenditem(item, &matches, &matchend);
-				number_of_matches++;
-			}
-		}
+		for (i = 0; i < tokc; i++)
+			if (!fstrstr(item->text, tokv[i]))
+				break;
+		if (i != tokc) /* not all tokens match */
+			continue;
+		/* exact matches go first, then prefixes, then substrings */
+		if (!tokc || !fstrncmp(text, item->text, textsize))
+			appenditem(item, &matches, &matchend);
+		else if (!fstrncmp(tokv[0], item->text, len))
+			appenditem(item, &lprefix, &prefixend);
 		else
-			appenditem(item, &matches, &matchend);
+			appenditem(item, &lsubstr, &substrend);
 	}
-
-	if (number_of_matches) {
-		/* initialize array with matches */
-		if (!(fuzzymatches = realloc(fuzzymatches, number_of_matches * sizeof(struct item*))))
-			die("cannot realloc %u bytes:", number_of_matches * sizeof(struct item *));
-		for (i = 0, item = matches; item && i < number_of_matches; i++, item = item->right)
-			fuzzymatches[i] = item;
-
-		/* sort matches according to distance */
-		qsort(fuzzymatches, number_of_matches, sizeof(struct item *), compare_distance);
-		/* rebuild list of matches */
-		matches = matchend = NULL;
-		for (i = 0, item = fuzzymatches[0]; i < number_of_matches && item && \
-				item->text; item = fuzzymatches[i], i++)
-			appenditem(item, &matches, &matchend);
-
-		free(fuzzymatches);
+	if (lprefix) {
+		if (matches) {
+			matchend->right = lprefix;
+			lprefix->left = matchend;
+		} else
+			matches = lprefix;
+		matchend = prefixend;
+	}
+	if (lsubstr) {
+		if (matches) {
+			matchend->right = lsubstr;
+			lsubstr->left = matchend;
+		} else
+			matches = lsubstr;
+		matchend = substrend;
 	}
 	curr = sel = matches;
 	calcoffsets();
 }
-
 
 static void
 insert(const char *str, ssize_t n)
@@ -300,7 +274,7 @@ insert(const char *str, ssize_t n)
 	if (n > 0)
 		memcpy(&text[cursor], str, n);
 	cursor += n;
-	fuzzymatch();
+	match();
 }
 
 static size_t
@@ -369,7 +343,7 @@ keypress(XKeyEvent *ev)
 
 		case XK_k: /* delete right */
 			text[cursor] = '\0';
-			fuzzymatch();
+			match();
 			break;
 		case XK_u: /* delete left */
 			insert(NULL, 0 - cursor);
@@ -518,7 +492,7 @@ insert:
 		strncpy(text, sel->text, sizeof text - 1);
 		text[sizeof text - 1] = '\0';
 		cursor = strlen(text);
-		fuzzymatch();
+		match();
 		break;
 	}
 
@@ -679,7 +653,7 @@ setup(void)
 	}
 	promptw = (prompt && *prompt) ? TEXTW(prompt) - lrpad / 4 : 0;
 	inputw = MIN(inputw, mw/3);
-	fuzzymatch();
+	match();
 
 	/* create menu window */
 	swa.override_redirect = True;
